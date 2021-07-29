e50db958a1b7dcef49bb1d9f679e526095904bff
KartikJondhalekar <jondhalekarkartik@gmail.com>
2021-07-29 12:46:24 +0530

    periodic check done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace JenkinsGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
